1aa65d05e1a4317eddc01f1f62f13b571e79728a
Emmanuel Osuolale <dmarinere@gmail.com>
2022-07-02 23:04:31 +0100

    first version

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const Network = require('netty_finder')
+
+
+
+
+
+app.post('/api/:number', (req, res) => {
+    let number = new Network(req.params.number);
+    const network = number.getNetworkName();
+    if (network !== null) {
+        res.send( `${number} belong to  the ${network} network`);
+    }
+    else {
+        res.send("The number you just sent does not belong to any network");   
+        }
+    }
+);
+
+const port = process.env.PORT || '3000'
+app.listen(port, () => console.log(`API running on localhost:${port}`))
